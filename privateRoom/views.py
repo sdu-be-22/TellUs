c3ffc55a848d3e58232226923eb8c733d0d3b884
yrysNM <yrys.n3m@gmail.com>
2022-05-19 21:27:09 +0600

    fixed bugs chat follows

--- privateRoom/views.py
+++ privateRoom/views.py
@@ -8,29 +8,30 @@ User = get_user_model()
 
 @login_required
 def index(request):
-    #users = User.objects.exclude(username=request.user.username)
-    users_profile = User.objects.get(id=request.user.id)
+    users = User.objects.exclude(username=request.user.username)
+    users_profile = UserProfile.objects.get(user_id=request.user.id)
     user_followers= users_profile.followers.all()
-    for  user_follower in  user_followers:
-        print(user_follower.user)
-        users = User.objects.filter(username=user_follower.user.username)
+
+    # for  user_follower in  user_followers:
+    #     print(user_follower)
+    #     users = User.objects.filter(username=user_follower)
 
     return render(request, 'privateChat/chats.html', context={'users': users, "user_followers": user_followers})
 
 @login_required
 def chatPage(request, username):
     user_obj = User.objects.get(username=username)
-    users_profile = User.objects.get(id=request.user.id)
+    users_profile = UserProfile.objects.get(user_id=request.user.id)
     user_followers = users_profile.followers.all()
 
-    for user_follower in user_followers:
-         users = User.objects.filter(username=user_follower.user)
+    # for user_follower in user_followers:
+    #      users = User.objects.filter(username=user_follower.user)
 
-    #users = User.objects.exclude(username=request.user.username)
+    users = User.objects.exclude(username=request.user.username)
 
     if request.user.id > user_obj.id:
         thread_name = f'chat_{request.user.id}-{user_obj.id}'
     else:
         thread_name = f'chat_{user_obj.id}-{request.user.id}'
     message_objs = ChatModel.objects.filter(thread_name=thread_name)
-    return render(request, 'privateChat/main_chat.html', context={'user': user_obj, 'users': users, 'messages': message_objs})
+    return render(request, 'privateChat/main_chat.html', context={'user': user_obj, 'users': users, 'messages': message_objs, "user_followers": user_followers})
