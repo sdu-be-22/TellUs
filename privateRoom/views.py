1c3be55bd8eb3ef36bc3e9a7b69df98db95b37a1
yrysNM <yrys.n3m@gmail.com>
2022-05-19 21:05:34 +0600

    chat with followers

--- privateRoom/views.py
+++ privateRoom/views.py
@@ -2,19 +2,31 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from .models import ChatModel
-
+from core.models import UserProfile
 
 User = get_user_model()
 
 @login_required
 def index(request):
-    users = User.objects.exclude(username=request.user.username)
-    return render(request, 'privateChat/chats.html', context={'users': users})
+    #users = User.objects.exclude(username=request.user.username)
+    users_profile = User.objects.get(id=request.user.id)
+    user_followers= users_profile.followers.all()
+    for  user_follower in  user_followers:
+        print(user_follower.user)
+        users = User.objects.filter(username=user_follower.user.username)
+
+    return render(request, 'privateChat/chats.html', context={'users': users, "user_followers": user_followers})
 
 @login_required
 def chatPage(request, username):
     user_obj = User.objects.get(username=username)
-    users = User.objects.exclude(username=request.user.username)
+    users_profile = User.objects.get(id=request.user.id)
+    user_followers = users_profile.followers.all()
+
+    for user_follower in user_followers:
+         users = User.objects.filter(username=user_follower.user)
+
+    #users = User.objects.exclude(username=request.user.username)
 
     if request.user.id > user_obj.id:
         thread_name = f'chat_{request.user.id}-{user_obj.id}'
